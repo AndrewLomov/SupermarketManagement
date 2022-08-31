bc26bc10a0fdf5a56cd83ff42285964f381c09b0
Andrew <andre-lomov25@mail.ru>
2022-09-01 00:38:30 +0300

    Added Cashier's console for selling products

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -49,6 +49,8 @@ namespace WebApp
             services.AddTransient<IEditProductUseCase, EditProductUseCase>();
             services.AddTransient<IGetProductByIdUseCase, GetProductByIdUseCase>();
             services.AddTransient<IDeleteProductUseCase, DeleteProductUseCase>();
+            services.AddTransient<IViewProductsByCategoryId, ViewProductsByCategoryId>();
+            services.AddTransient<ISellProductUseCase, SellProductUseCase>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
